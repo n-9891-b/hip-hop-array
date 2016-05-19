69f74bab3fd73c64ec34448382f94d63cfe47b75
Nate Blain <blain89@gmail.com>
2016-05-19 10:28:16 -0700

    [feat] create photoController.js request-handler.js requests.js and router.js

--- server/config/router.js
+++ server/config/router.js
@@ -1,7 +1,8 @@
 var bodyParser = require('body-parser');
+var requestHandler = ('./request-handler.js');
 
 module.exports = function (app) {
 
-app.post();
+app.post('/recipes', requestHandler.photoHandler);
 
 };
