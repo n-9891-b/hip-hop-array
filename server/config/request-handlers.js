29dbfb65a8d04844f6bb39e55fcc0c9f886a9b59
Nate Blain <blain89@gmail.com>
2016-05-20 09:15:43 -0700

    [fix] merge conflict

--- server/config/request-handlers.js
+++ server/config/request-handlers.js
@@ -7,14 +7,9 @@ module.exports = {
     // var imageURL1 = req.body.imageURL1;
     // var imageURL2 = req.body.imageURL2;
     var imageURL1 = req.body.imageUrl;//"https://upload.wikimedia.org/wikipedia/commons/8/88/Bright_red_tomato_and_cross_section02.jpg"; req.body.image
-<<<<<<< HEAD
-    console.log('imageUrl', imageURL1)
-=======
->>>>>>> [chore] hook up request handlers to client
-    request.photoAnalysisReq(imageURL1);
-=======
+
+
     req.photoAnalysisReq(imageURL1);
->>>>>>> [fix] fixed errors
     //return compiled
   }
 };
