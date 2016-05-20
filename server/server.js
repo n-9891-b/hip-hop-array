953d7737a9514c6966e9e3cc603c9455fc1bd3f2
Gina Zhou <ginayizhou@gmail.com>
2016-05-19 17:01:26 -0700

    [fix] modularized the front-end foldering structure

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ var port = process.env.PORT || 4568;
 var app = express();
 
 app.use(bodyParser.json());
-app.use(express.static(__dirname + '/../client/views'));
+app.use(express.static(__dirname + '/../client/'));
 
 app.listen(port, function(){
   console.log('Listening on port ' + port + '!');
