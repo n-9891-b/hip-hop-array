45d2bee3245fa3d2f914e791b961c6fba0ea2048
Nate Blain <blain89@gmail.com>
2016-05-20 16:01:07 -0700

    [feat] add upload function

--- client/app/controllers/fileUpload.js
+++ client/app/controllers/fileUpload.js
@@ -10,26 +10,14 @@ angular.module('app.fileUpload', ['ngFileUpload'])
     });
   };
 
-  $scope.uploadFiles = function(files, errFiles) {
-    $scope.files = files;
-    $scope.errFiles = errFiles;
-    angular.forEach(files, function(file) {
-      file.upload = Upload.upload({
-        url: 'https://angular-file-upload-cors-srv.appspot.com/upload',
-        data: {file: file}
+  $scope.uploadFiles = function(files) {  //, errFiles
+    if (files && files.length) {
+      console.log(files);
+      console.log(files[0].constructor);
+      Upload.upload({
+        url: '/api/photo',
+        data: {files: files}
       });
-
-      file.upload.then(function (response) {
-        $timeout(function () {
-          console.log(response.data);
-          file.result = response.data;
-        });
-      }, function (response) {
-        if (response.status > 0)
-          $scope.errorMsg = response.status + ': ' + response.data;
-      }, function (evt) {
-        file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
-      });
-    });
-  };
+    };
+  }
 });
