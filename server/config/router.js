8eb6e79ad1274cab0e63bdbb0a79daf8003032af
Nate Blain <blain89@gmail.com>
2016-05-20 16:08:08 -0700

    [chore] add new post route to router.js

--- server/config/router.js
+++ server/config/router.js
@@ -3,6 +3,9 @@ var requestHandler = require('./request-handlers.js');
 
 module.exports = function (app) {
   app.post('/recipes', requestHandler.photoHandler);
+  app.post('/ingredients', upload.array('photos', 12), function (req, res) {
+    console.log(req);
+  });
 };
 
 
