68ebda6576fe87722cbbb188b0326c4165b45957
Nate Blain <blain89@gmail.com>
2016-05-23 17:05:23 -0700

    [fix] asynchronous issue with promises

--- server/config/request-handlers.js
+++ server/config/request-handlers.js
@@ -1,15 +1,13 @@
 var bodyParser = require('body-parser');
 var request = require('./requests.js');
-var fs = require('fs');
 
 module.exports = {
   photoHandler: function (req, res) {
-    req.files.forEach(function (file) {
-      var image = file.path;
-      var imagePath = '/../../' + image;
-      fs.readFile(__dirname + imagePath, function(err, imageFile){
-        request.photoAnalysisReq(imageFile, res);
-      });
+    Promise.all(req.files.map(function(item) {
+      return request.photoAnalysisReq('/../../' + item.path);
+    }))
+    .then(function(resultsArray) {
+      res.send(resultsArray);
     });
   }
 };
--- server/config/requests.js
+++ server/config/requests.js
@@ -1,36 +1,24 @@
 var request = require('request');
 var requestHandler = require('./request-handlers');
 var Promise = require('bluebird');
+var fs = require('fs');
 var AlchemyAPI = require('alchemy-api');
 var alchemy = new AlchemyAPI('82ef262b9654e3ac400ff00238a1cc588c309e9c');
 var FoodToForkAPI = '6c217911dc3551a37654ed22d97dabdb';
 
 module.exports = {
-  photoAnalysisReq: function (image, res) {
-<<<<<<< HEAD
-      // images.forEach(alchemy(image))
-      
-      return new Promise (function (resolve, reject) {
-        alchemy.imageKeywords(image, {}, function (err, response) {
-        if (err) throw err;
-        var imageKeywords = response.imageKeywords;
-        var foodType = imageKeywords[0].text;
-        resolve(foodType);
-        });
-      })
-=======
+  photoAnalysisReq: function (imagePath) {
     return new Promise (function (resolve, reject) {
-      alchemy.imageKeywords(image, {}, function (err, response) {
-      if (err) throw err;
-      var imageKeywords = response.imageKeywords;
-      var foodType = imageKeywords[0].text;
-      resolve(foodType);
+      fs.readFile(__dirname + imagePath, function(err, imageFile){
+        if(err) throw err;
+        alchemy.imageKeywords(imageFile, {}, function (err, response) {
+          if (err) throw err;
+          var imageKeywords = response.imageKeywords;
+          var foodType = imageKeywords[0].text;
+          resolve(foodType);
+        });
       });
     })
->>>>>>> [chore] implement photo upload streaming and analysis
-    .then(function(foodType) {
-      res.send(foodType);
-    });
   },
 
   recipesReq: function (reqInfo, res) {
