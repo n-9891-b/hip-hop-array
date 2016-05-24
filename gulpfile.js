9a30c26877305241356cadd0f91e0b82da213867
Adam Behrens <behrens.adam@gmail.com>
2016-05-23 17:26:47 -0700

    [fix] bower install for heroku

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp'),
+  concat = require('gulp-concat'),
+  uglify = require('gulp-uglify'),
+  rename = require('gulp-rename'),
+  bower = require('gulp-bower');
+
+// gulp.task('bower', function() { 
+//     return bower()
+//     .pipe(gulp.dest(config.bowerDir)) 
+// })
+
+gulp.task('client-scripts', function() {
+    // return gulp.src('client/app/*.js')
+    //   .pipe(concat('client-main.js'))
+    //     .pipe(rename({suffix: '.min'}))
+    //     .pipe(uglify())
+    //     .pipe(gulp.dest('build/js'));
+});
+gulp.task('server-scripts', function() {
+    // return gulp.src('server/*.js')
+    //   .pipe(concat('server-main.js'))
+    //     .pipe(rename({suffix: '.min'}))
+    //     .pipe(uglify())
+    //     .pipe(gulp.dest('build/js'));
+});
+
+// Default Task
+gulp.task('default', ['client-scripts', 'server-scripts']);
