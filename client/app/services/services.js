c5bb67f4ebbd0b88f610e66d7c679209f0b4ca88
Gina Zhou <ginayizhou@gmail.com>
2016-05-24 21:47:12 -0700

    [feat] test favicon and angular ngrouter resolve funtionality

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -2,45 +2,12 @@
 
 angular.module('app.services', [])
 
-.factory('imageUrlFactory', function($http) {
-  // function sendUrl(url, cb) {
-  //   return $http({
-  //     method: 'POST', 
-  //     url: '/api/photo/recipes',
-  //     data: {imageUrl: url}
-  //   })
-  //   .then(function success(response) {
-  //     cb(response.data);
-  //   })
-  //   .catch(function() {
-  //     console.error('error during ingredient API fetch');
-  //   })
-  // }
-
-  function sendFiles(imagesArr, cb){
-    // return $http({
-    //   method: 'POST',
-    //   url: '/api/photo/recipes',
-    //   data: {imageUrl: imagesArr}
-    // })
-    // .then(function success(response) {
-    //   cb(response.data);
-    // })
-    // .catch(function() {
-    //   console.error('error during ingredient API fetch');
-    // })
-  }
-
-  return {
-    // sendUrl: sendUrl,
-    sendFiles: sendFiles
-  };
-})
-
 .factory('recipesFactory', function() {
+  var totalRecipes;
   var myRecipesStorage = [];
 
   return {
+    totalRecipes: totalRecipes,
     myRecipesStorage: myRecipesStorage
   };
 });
