40c80394de7b1d9145696f9b776673314ad13ee0
Gina Zhou <ginayizhou@gmail.com>
2016-05-31 12:25:04 -0700

    [feat] remove recipes from MyRecipes

--- client/app/controllers/myRecipes.js
+++ client/app/controllers/myRecipes.js
@@ -2,6 +2,16 @@
 
 angular.module('app.myRecipes', [])
 
-.controller('myRecipesCtrl', function($scope, recipesFactory) {
+.controller('myRecipesCtrl', function($scope, recipesFactory, $location) {
   $scope.myRecipesStorage = recipesFactory.myRecipesStorage;
+
+  $scope.removeFromMyRecipes = function(recipe) {
+    var index = recipesFactory.myRecipesStorage.indexOf(recipe);
+    recipesFactory.myRecipesStorage.splice(index, 1);
+    $scope.myRecipesStorage = recipesFactory.myRecipesStorage;
+
+    if (recipesFactory.myRecipesStorage.length === 0) {
+      $location.path('/error');
+    }
+  }
 });
--- client/app/controllers/recipes.js
+++ client/app/controllers/recipes.js
@@ -4,10 +4,16 @@ angular.module('app.recipes', [])
 
 .controller('recipesCtrl', function($scope, $location, recipesFactory) {
   $scope.totalRecipes = recipesFactory.totalRecipes;
-  
+  $scope.isActive = false;
+
   $scope.addToList = function(item){
-    if (recipesFactory.myRecipesStorage.indexOf(item) === -1) {
+    if (recipesFactory.myRecipesStorage.indexOf(item) === -1 && !item.isActive) {
       recipesFactory.myRecipesStorage.push(item);
+      item.isActive = true;
+    } else {
+      var index = recipesFactory.myRecipesStorage.indexOf(item);
+      recipesFactory.myRecipesStorage.splice(index, 1);
+      delete item.isActive;
     }
   };
 });
